db264a179b18691dda739d7a6471c7dc96fd691d
Murodbey <abdullayevmurod@gmail.com>
2019-03-28 12:11:50 -0500

    svr file deleted

--- k2tf-vault-deployment.tf
+++ k2tf-vault-deployment.tf
@@ -66,4 +66,21 @@ resource "kubernetes_deployment" "vault" {
     }
   }
 }
+resource "kubernetes_service" "vault_service" {
+  metadata {
+    name      = "vault-service"
+    namespace = "test"
+  }
+  
+  spec {
+    selector { app = "vault-deployment"
+    }
 
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 8200
+    }
+    type = "LoadBalancer"
+  }
+}
