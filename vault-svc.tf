823897542dbbf5c565079ddc51834787eeea13ba
Murodbey <abdullayevmurod@gmail.com>
2019-03-28 11:40:56 -0500

    port issue fixed

--- vault-svc.tf
+++ vault-svc.tf
@@ -10,7 +10,7 @@ resource "kubernetes_service" "vault_service" {
     port {
       name        = "vault"
       port        = 80
-      target_port = "8200"
+      target_port = 8200
     }
     type = "LoadBalancer"
   }
