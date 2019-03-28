356a1bd2eda6cf1f8c149fc2cc7457e7b5dc35a2
Murodbey <abdullayevmurod@gmail.com>
2019-03-28 11:32:32 -0500

    service file added

--- k2tf-vault-deployment.tf
+++ k2tf-vault-deployment.tf
@@ -1,20 +1,3 @@
-resource "kubernetes_service" "vault_service" {
-  metadata {
-    name      = "vault-service"
-    namespace = "test"
-    labels {
-      app = "vault-deployment"
-    }
-  }
-  spec {
-    port {
-      name        = "vault"
-      port        = 80
-      target_port = "8200"
-    }
-    type = "LoadBalancer"
-  }
-}
 resource "kubernetes_persistent_volume_claim" "vault_pvc" {
   metadata {
     name      = "vault-pvc"
---
+++ vault-svc.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_service" "vault_service" {
+  metadata {
+    name      = "vault-service"
+    namespace = "test"
+    labels {
+      app = "vault-deployment"
+    }
+  }
+  spec {
+    port {
+      name        = "vault"
+      port        = 80
+      target_port = "8200"
+    }
+    type = "LoadBalancer"
+  }
+}
