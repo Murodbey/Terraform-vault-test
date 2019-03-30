3b38bf5348611316e12367f2d84c2de36d938941
Murodbey <abdullayevmurod@gmail.com>
2019-03-30 16:55:31 -0500

    namespace updated

--- vault-secrets.tf
+++ vault-secrets.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_secret" "vault_secret" {
   metadata {
     name      = "vault-secret"
-    namespace = "tools"
+    namespace = "test"
   }
   data {
     password = "TXlWM3J5UzNjdXJlUGFzU3dE"
