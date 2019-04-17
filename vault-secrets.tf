894ed844a87a6016d518230f0dca62199f63c0a0
Murodbey <abdullayevmurod@gmail.com>
2019-04-17 14:18:24 -0500

    tfvars added

--- vault-secrets.tf
+++ vault-secrets.tf
@@ -1,10 +1,10 @@
-resource "kubernetes_secret" "vault_secret" {
-  metadata {
-    name      = "vault-secret"
-    namespace = "test"
-  }
-  data {
-    password = "MyV3ryS3curePasSwD"
-  }
-  type = "Opaque"
-}
+# resource "kubernetes_secret" "vault_secret" {
+#   metadata {
+#     name      = "vault-secret"
+#     namespace = "test"
+#   }
+#   data {
+#     password = "MyV3ryS3curePasSwD"
+#   }
+#   type = "Opaque"
+# }
